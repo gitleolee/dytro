fada1b6b6c9334924118df303350f9009d670643
mikey1384 <mikey1384@gmail.com>
2018-09-15 17:38:20 +0700

    react router added (this is what you use in react for links instead of <a> tag), updated Readme - please read

---
+++ src/Header.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { css } from 'emotion';
+import { Link } from 'react-router-dom';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div
+        className={css`
+          display: flex;
+          flex-direction: row-reverse;
+          align-items: center;
+          height: 4rem;
+          background: #00b9ed;
+          a {
+            margin-right: 1rem;
+            text-decoration: none;
+            color: #fff;
+          }
+        `}
+      >
+        <div
+          className={css`
+            a {
+              margin-right: 1rem;
+              text-decoration: none;
+            }
+          `}
+        />
+        <Link to="/">Home</Link>
+        <Link to="/login">Login</Link>
+      </div>
+    );
+  }
+}
