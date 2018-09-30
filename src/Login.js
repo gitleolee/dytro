0b817254e40c51a595842d78c7238c3c7200b062
mikey1384 <mikey1384@gmail.com>
2018-09-30 19:14:10 +0700

    made changes to password algorithm

--- src/Login.js
+++ src/Login.js
@@ -1,39 +1,81 @@
-import React, { Component } from "react";
-import { css } from "emotion";
+import React, { Component } from 'react';
+import { css } from 'emotion';
 
 export default class Login extends Component {
+  state = {
+    username: '',
+    password: ''
+  };
+
   render() {
+    const username = this.state.username;
+    const password = this.state.password;
+
     return (
       <div
-        className={css`
-          input {
-            margin-botton: 10px;
-            width: 130px;
-            height: 25px;
-            margin-top: 5px;
-            margin-left: 10px;
-          }
-
-          #container {
-            margin-top: 100px;
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <div
+          style={{
+            marginTop: '6rem'
+          }}
+          className={css`
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
             height: 500px;
             width: 400px;
             background-color: aqua;
             border-radius: 25px;
-            margin-left: 500px;
-          }
-
-          #name {
-            margin-top: 250px;
-          }
-        `}
-      >
-        <div id="container">
-          <input id="name" placeholder="Username" type="text" maxLength={15} />
-          <br />
-          <input placeholder="Password" type="password" maxLength={10} />
+            input {
+              width: 20rem;
+              height: 25px;
+            }
+          `}
+        >
+          <div>
+            <input
+              placeholder="Username"
+              type="text"
+              maxLength={15}
+              value={username}
+              onChange={event =>
+                this.setState({ username: event.target.value })
+              }
+            />
+            <br />
+            <input
+              placeholder="Password"
+              type="password"
+              maxLength={10}
+              value={password}
+              onChange={event =>
+                this.setState({ password: event.target.value })
+              }
+            />
+          </div>
+          <div>
+            <button
+              style={{ marginTop: '1rem', padding: '1rem', fontSize: '1.5rem' }}
+              onClick={this.checkIfPasswordIsCorrect}
+            >
+              Log In!
+            </button>
+          </div>
         </div>
       </div>
     );
   }
+
+  checkIfPasswordIsCorrect = () => {
+    const { password, username } = this.state;
+
+    console.log(password);
+    console.log(username);
+  };
 }
