eccdde9e7c2d8ae21395d03e874e7de0c1b0293f
mikey1384 <mikey1384@gmail.com>
2018-11-04 18:47:10 +0700

    checked state added

--- src/components/minecraft/Villager.js
+++ src/components/minecraft/Villager.js
@@ -2,7 +2,12 @@ import React, { Component } from 'react';
 import { css } from 'emotion';
 
 export default class VillagerGen extends Component {
+  state = {
+    checked: false
+  };
+
   render() {
+    const { checked } = this.state;
     return (
       <div
         className={css`
@@ -11,13 +16,20 @@ export default class VillagerGen extends Component {
           align-items: center;
           flex-direction: column;
           h1 {
-            color: aqua;
+            color: ${checked ? 'red' : 'aqua'};
           }
         `}
       >
         <h1>Villager Generator</h1>
-        Current Position <input type="checkbox" />
-        <h3 id="Output">/summon minecraft:villager ~ ~ ~ </h3>
+        Current Position{' '}
+        <input
+          value={checked}
+          onChange={() => this.setState(state => ({ checked: !state.checked }))}
+          type="checkbox"
+        />
+        <h3 id="Output">
+          /summon minecraft:villager {checked ? '1 2 3' : '~ ~ ~'}{' '}
+        </h3>
       </div>
     );
   }
