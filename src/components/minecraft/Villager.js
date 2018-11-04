1d33d7dffddd66c542882bb702f64d677e7ba2a9
BuildTools <soy1790@gmail.com>
2018-11-04 21:41:40 +0900

    VillagerGenIsHere

--- src/components/minecraft/Villager.js
+++ src/components/minecraft/Villager.js
@@ -3,11 +3,16 @@ import { css } from 'emotion';
 
 export default class VillagerGen extends Component {
   state = {
-    checked: false
+    checked: false,
+    xPos: '0',
+    yPos: '0',
+    zPos: '0',
+    nbtE: false,
+    name: ['']
   };
 
   render() {
-    const { checked } = this.state;
+    const { checked, xPos, yPos, zPos, nbtE, name } = this.state;
     return (
       <div
         className={css`
@@ -16,7 +21,7 @@ export default class VillagerGen extends Component {
           align-items: center;
           flex-direction: column;
           h1 {
-            color: ${checked ? 'red' : 'aqua'};
+            color: aqua;
           }
         `}
       >
@@ -32,8 +37,40 @@ export default class VillagerGen extends Component {
             type="checkbox"
           />
         </div>
+        <input
+          placeholder="X Pos"
+          value={xPos}
+          onChange={e => this.setState({ xPos: e.target.value })}
+        />
+        <input
+          placeholder="Y Pos"
+          value={yPos}
+          onChange={e => this.setState({ yPos: e.target.value })}
+        />
+        <input
+          placeholder="Z Pos"
+          value={zPos}
+          onChange={e => this.setState({ zPos: e.target.value })}
+        />
+        <div style={{ display: 'flex' }}>
+          <div>Enable NBT</div>
+          <input
+            style={{ marginLeft: '0.5rem' }}
+            value={nbtE}
+            onChange={() => this.setState(state => ({ nbtE: !state.nbtE }))}
+            type="checkbox"
+          />
+        </div>
+        <input
+          placeholder="Enter Name"
+          value={name}
+          onChange={e => this.setState({ name: [e.target.value] })}
+        />
         <h3 id="Output">
-          /summon minecraft:villager {checked ? '1 2 3' : '~ ~ ~'}{' '}
+          /summon minecraft:villager{' '}
+          {checked ? '~ ~ ~' : [xPos, ' ', yPos, ' ', zPos]} {nbtE && ['{']}
+          {nbtE && ['CustomName:"{\\"text\\":\\"', name, '\\"}"']}
+          {nbtE && '}'}
         </h3>
       </div>
     );
