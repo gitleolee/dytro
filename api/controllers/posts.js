e050e0f0ec69585cd554e2644a02165aa5641215
DytroCodes <soy1790@gmail.com>
2019-09-07 20:11:00 +0900

    BlackHeader

--- api/controllers/posts.js
+++ api/controllers/posts.js
@@ -5,7 +5,7 @@ const { poolQuery } = require('../helpers');
 router.get('/', async(req, res) => {
   try {
     const messages = await poolQuery(
-      `SELECT a.id, a.userId, a.message, b.username FROM posts a JOIN users b ON a.userId = b.id`
+      `SELECT a.id, a.userId, a.message, b.username FROM posts a JOIN users b ON a.userId = b.id ORDER BY a.id`
     );
     res.send(messages);
   } catch (error) {
@@ -14,7 +14,7 @@ router.get('/', async(req, res) => {
 });
 
 router.post('/', async(req, res) => {
-  console.log(req.body)
+  console.log(req.body);
   const result = await poolQuery(`INSERT INTO posts SET ?`, {
     userId: req.body.userId,
     message: req.body.text
