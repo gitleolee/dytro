1af76569e553454d00d9e648afed400cbd4c766e
mikey1384 <mikey1384@gmail.com>
2019-09-13 19:17:42 +0800

    fix service worker issue

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
@@ -11,4 +11,4 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById('root')
 );
-registerServiceWorker();
+serviceWorker.unregister();
